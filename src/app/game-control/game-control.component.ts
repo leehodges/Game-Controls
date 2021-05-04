9ba2f1c34e19f8e990f31d449ab7e6940e17a61e
Lee Hodges <leehodges@gmail.com>
2021-05-04 10:39:18 -0500

    Enhancements
    Start/Pause buttons will be disabled when a user clicks start or pause appropriately.
    
    Numbers will now be displayed in order 1-2-3-4  etc...

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,29 +1,36 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from "@angular/core";
 
 @Component({
-  selector: 'app-game-control',
-  templateUrl: './game-control.component.html',
-  styleUrls: ['./game-control.component.css']
+  selector: "app-game-control",
+  templateUrl: "./game-control.component.html",
+  styleUrls: ["./game-control.component.css"],
 })
 export class GameControlComponent implements OnInit {
   @Output() intervalFired = new EventEmitter<number>();
   interval;
   lastNumber = 0;
+  // Set a start and pause. Set start to false as we want the button enabled and pause to true as we want it to start disabled
+  start = false;
+  pause = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   onStartGame() {
     this.interval = setInterval(() => {
       this.intervalFired.emit(this.lastNumber + 1);
       this.lastNumber++;
     }, 1000);
+    // When we click start game set start to true and pause to false to disable start and enable pause
+    this.start = true;
+    this.pause = false;
   }
 
   onPauseGame() {
     clearInterval(this.interval);
+    // do the reverse as we did above
+    this.start = false;
+    this.pause = true;
   }
-
 }
